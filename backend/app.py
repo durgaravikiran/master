161cbd70259573c3ebc6887ae26e681a5e6512a0
Nityaanand12 <nityaanandchakradhar@gmail.com>
2020-07-06 23:02:42 +0530

    graph post call added

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,16 @@ def func():
 def func1():
     return {"message":{"x":10,"y":20}}
 
+@app.route("/graph", methods=['POST'])
+def func2():
+    return {"message":[
+	{"a":{"x":10,"y":20}},
+	{'b':{"x":10,"y":20}},
+	{'c':{"x":10,"y":20}},
+	{'d':{"x":10,"y":20}},
+	{'e':{"x":10,"y":20}},
+	{'f':{"x":10,"y":20}},
+	{'g':{"x":10,"y":20}}]}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001)
